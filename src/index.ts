0095b03358fa0ada92f2d794e9915b9464154f14
Charles F <charles.f@visailabs.com>
2025-05-12 01:59:43 +0530

    added health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import express from 'express';
-import userRoutes from './routes/user.route';
+import express from "express";
+import userRoutes from "./routes/user.route";
 import cors from "cors";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
-app.use(cors({ origin: "*" }))
+app.use(cors({ origin: "*" }));
 
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
+
+app.get("/health", (req, res) => {
+  return res.send("Alive");
+});
+
 app.listen(port, () => {
   console.log(`Backend listening at http://localhost:${port}`);
 });
